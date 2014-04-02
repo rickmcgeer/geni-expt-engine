cfd8544c24bbc4d01558f8215d54d8b50e703a5e
rickmcgeer <rick@mcgeer.com>
2014-04-02 15:14:28 -0700

    Added scripts to repo

--- igplc-scripts/fabfile.py
+++ igplc-scripts/fabfile.py
@@ -1,13 +1,13 @@
 from fabric.api import local
 host = 'bilby.cs.princeton.edu'
 user = 'service_instageni'
-remote_dir = '/home/service_instageni/gee-master'
+remote_dir = '/home/service_instageni/'
 #
 # change this to your private key...
 #
 keyfile = '/Users/rick/.ssh/id_dsa'
 def deploy():
-    local('scp -i %s -r *.js start-gee-master.sh views public package.json %s@%s:%s' % (keyfile, user, host, remote_dir))
+    local('scp -i %s -r *.plcsh' % (keyfile, user, host, remote_dir))
 
 def deploy_file(file):
     local('scp -i %s -r %s %s@%s:%s/%s' % (keyfile, file, user, host, remote_dir, file))
