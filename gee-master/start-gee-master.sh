9379fb8a2101dcf1539ea1d96a6bf8a4e92f55f4
rickmcgeer <rick@mcgeer.com>
2014-05-24 19:03:22 -0700

    Put in absolute path to start node, to avoid confusion with /usr/sbin/node, which is something else entirely

--- gee-master/start-gee-master.sh
+++ gee-master/start-gee-master.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script to start the GEE webserver
-nohup node app.js >> gee_console.log 2>> gee_console_error.log &
+nohup /usr/local/bin/node app.js >> gee_console.log 2>> gee_console_error.log &
