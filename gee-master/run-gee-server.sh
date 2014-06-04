11edb4d6a9b4c5f25dc23c41b263d245371e8b5f
rickmcgeer <rick@mcgeer.com>
2014-06-04 09:25:10 -0700

    fixed bug in start script: writing to the wrong error log

--- gee-master/run-gee-server.sh
+++ gee-master/run-gee-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true; do
-  /usr/local/bin/node app.js >> gee_console.log 2>> gee_error_console.log
+  /usr/local/bin/node app.js >> gee_console.log 2>> gee_console_error.log
   # When you get here the process has died.  start
   # the loop again and restart it
 done
