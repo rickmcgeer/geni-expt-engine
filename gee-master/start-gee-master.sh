87c0579b42a5898a62c12b808777ea0e37edaca8
rickmcgeer <rick@mcgeer.com>
2014-06-14 19:19:48 -0700

    modified to use the environment variable GEE_CONFIG_FILE for location of plcsh scripts and info for them, replacing buggy location-based config.py stuff (though the config file is still config.py -- we are just pulling the path out of the environment and setting it in the startup scripts/fabfiles for test commands

--- gee-master/start-gee-master.sh
+++ gee-master/start-gee-master.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # script to start the GEE webserver
+export GEE_CONFIG_FILE='/home/service_instageni/config.py'
 nohup /usr/local/bin/node app.js >> gee_console.log 2>> gee_console_error.log &
--- igplc-scripts/fabfile.py
+++ igplc-scripts/fabfile.py
@@ -2,54 +2,29 @@ from fabric.api import local, task
 host = 'bilby.cs.princeton.edu'
 user = 'service_instageni'
 remote_dir = '/home/service_instageni/'
-remote_dir_test = '/home/service_instageni/test/'
-remote_dir_alternate = '/home/service_instageni/alternate/'
-config_file_standard = 'config.py.standard'
-config_file_alternate = 'config.py.alternate'
 
 #
 # change this to your private key...
 #
 keyfile = '/Users/rick/.ssh/id_dsa'
 
-def deploy_internal(remote_dir, config_file):
-    local('scp -i %s  *.plcsh %s@%s:%s' % (keyfile, user, host, remote_dir))
-    local('scp -i %s  %s %s@%s:%sconfig.py' % (keyfile, config_file, user, host, remote_dir))
-    local('scp -i %s  %s %s@%s:%sgee-master/config.py' % (keyfile, config_file, user, host, remote_dir))
-
-def deploy_file_internal(file, remote_dir, config_file):
-    if (file == "config.py"):
-        local('scp -i %s %s %s@%s:%sconfig.py' % (keyfile, config_file, user, host, remote_dir))
-        local('scp -i %s  %s %s@%s:%sgee-master/config.py' % (keyfile, config_file, user, host, remote_dir))
-    else:
-        local('scp -i %s -r %s %s@%s:%s/%s' % (keyfile, file, user, host, remote_dir, file))
-    
 @task
 def deploy():
-    deploy_internal(remote_dir, config_file_standard)
+    local('scp -i %s config.py  *.plcsh %s@%s:%s' % (keyfile, user, host, remote_dir))
+    local('scp -i %s config.py %s@%s:%sgee-master/config.py' % (keyfile,  user, host, remote_dir))
 
 @task
 def deploy_file(file):
-    deploy_file_internal(file, remote_dir, config_file_standard)
+    if (file == "config.py"):
+        local('scp -i %s config.py %s@%s:%sconfig.py' % (keyfile, user, host, remote_dir))
+        local('scp -i %s config.py %s@%s:%sgee-master/config.py' % (keyfile, user, host, remote_dir))
+    else:
+        local('scp -i %s -r %s %s@%s:%s/%s' % (keyfile, file, user, host, remote_dir, file))
+    
 
-@task
-def deploy_test():
-    deploy_internal(remote_dir_test, config_file_standard)
-
-@task
-def deploy_file_test(file):
-    deploy_file_internal(file, remote_dir_test, config_file_standard)
     
-@task 
-def deploy_alternate():
-    deploy_internal(remote_dir_alternate, config_file_alternate)
-    
-@task
-def deploy_file_alternate(file):
-    deploy_file_internal(file, remote_dir_alternate, config_file_alternate)
-    
-def test_gee_script(script, remote_dir):
-    run('cd %s; %s' % (remote_dir, script))
+def test_gee_script(script):
+    run('export GEE_CONFIG_FILE=%s/config.py; cd %s; %s' % (remote_dir, remote_dir, script))
     
 def form_command_line(script, user, slice):
     if (script == 'find-slicelets.plcsh'):
@@ -63,11 +38,7 @@ def form_command_line(script, user, slice):
 @task
 def test_script(script, user=None, slice=None):
     cmd = form_command_line(script, user, slice)
-    test_gee_script(cmd, remote_dir_standard)
+    test_gee_script(cmd)
 
-@task
-def test_script_alternate(script, user=None, slice=None):
-    cmd = form_command_line(script, user, slice)
-    test_gee_script(cmd, remote_dir_alternate)
                                    
     
