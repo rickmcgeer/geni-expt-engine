dce2b00701810104f033a486e9f503d9a2385882
Andy Bavier <acb@cs.princeton.edu>
2014-06-22 15:28:52 -0700

    Incorporate Rick's improvements.

--- igplc-scripts/user-scripts/fabfile.py
+++ igplc-scripts/user-scripts/fabfile.py
@@ -5,9 +5,120 @@ env.key_filename='~/.ssh/%SLICE%.pem'
 env.hosts = %HOSTS%
 slivers = %SLIVERS%
 
+#
+# Generate the GEE Programming Environment
+#
+
+
+@task
+def install_pip():
+    with settings (warn_only=True):
+	run("yum install -y python-pip")
+	run("ln -s /usr/bin/pip-python /usr/bin/pip")
+	
+#
+# A utility to pull the ip address for a given host
+#
+	
+def get_ip_addr(hostname):
+    ip_addresses = [sliver['addr'] for sliver in slivers if sliver['host'] == hostname]
+    if (len(ip_addresses) == 0): return ""
+    return ip_addresses[0]
+
+#
+# Generate and install the GEE Messaging Service (Beanstalk)
+#
+
+#
+# Set up defaults for the server
+#
+env.beanstalk_server_hostname = env.hosts[0]
+env.beanstalk_server = get_ip_addr(env.beanstalk_server_hostname)
+
+#
+# Install the server on all nodes
+#
+
+@task
+def install_beanstalk_server():
+    with settings (warn_only=True):
+	run("yum install -y beanstalkd")
+	
+#
+# install the client on all nodes
+#
+	
+@task
+def install_beanstalk_client():
+    with settings (warn_only=True):
+	run("yum install -y libyaml")
+	run("pip-python install pyyaml")
+	run("pip-python install beanstalkc")
+
+#
+# Install the GEE Programming Environment
+#
+@task
+def install_gee_environment():
+    install_pip()
+    install_beanstalk_server()
+    install_beanstalk_client()
+    
+#
+# Set up the beanstalk server and start it running on
+# env.beanstalk_server
+#
+    
+@task
+@hosts('localhost')
+def setup_beanstalk_server():
+    with settings(host_string=env.beanstalk_server_hostname):
+	install_beanstalk_server()
+	run('beanstalkd -l %s -p 14711 &' % env.beanstalk_server)
+	
+#
+# Check to make sure the server is running
+#
+
+@task
+@hosts(env.beanstalk_server_hostname)
+def check_beanstalk_server():
+    run ('netstat -tulpn')
+    
+#
+# Setup the beanstalk clients.  This generates and
+# copies to each client a beanstalk_config.py file,
+# which connects to the server and establishes that they are watching
+# the tube with their hostname.  After this, to use
+# beanstalk, all the client will have to do is
+# import('beanstalk_config.py') into his Python program
+#
+
+
+@task
+@hosts('localhost')
+def setup_beanstalk_clients():
+    if not env.beanstalk_server:
+	print "Warning: beanstalk_server must be set before clients can be set up"
+	print "Setting default beanstalk server"
+	env.beanstalk_server_hostname = env.hosts[0]
+	env.beanstalk_server = get_ip_addr(env.beanstalk_server_hostname)
+	setup_beanstalk_server()	
+    for host in env.hosts:
+	with settings(host_string = host):
+	    f = open("beanstalk_config.py", "w")
+	    f.write("import beanstalkc\n")
+	    f.write("beanstalk = beanstalkc.Connection(host='%s', port=14711)\n" % env.beanstalk_server)
+	    f.write("beanstalk.watch('%s')\n" % host)
+	    f.write("client_tubes = %s\n" % str(env.hosts))
+	    f.write("server_tube = 'server'\n")
+	    f.close()
+	    local("scp -i %s beanstalk_config.py %s@%s:." % (env.key_filename, env.user, host))
+
+
 # Install the 'nmap' RPM on all hosts
 @task
-def prepare():
+def install_nmap():
     run("sudo yum -qy install nmap")
 
 # Run 'nmap' on the private network on each host.
@@ -19,6 +130,10 @@ def nmap():
     run("echo")
     run("echo Hosts reachable on the private subnet:")
     run("nmap -sP 10.128.234.0/24")
+    
+#------------------------------------------------------------------------------------------------
+# Experimenter code here
+#
 
 # Your experiment script goes here...
 @task
@@ -27,3 +142,68 @@ def run_expt():
     # run('ifconfig -a | grep  inet | grep :10')
     # put the commands you want to run here...
     pass
+
+
+
+@hosts([env.hosts[0]])
+@task
+def install_scraper():
+    for host in env.hosts:
+	local('scp -i %s scrape-youtube.py %s@%s:.' % (env.key_filename, env.user, host))
+    
+
+
+@task
+def install_beautiful_soup():
+    with settings (warn_only=True):
+	run("pip install requests")
+	run("pip install beautifulsoup4")
+
+@task
+def install_youtube_dl():
+    with settings (warn_only=True):
+	run('pip install --upgrade youtube_dl')
+	
+@task
+def install_all():
+    install_gee_environment()
+    install_beautiful_soup()
+    install_scraper()
+    install_youtube_dl()
+    
+
+
+
+
+
+@task
+@hosts(env.hosts[0])
+def setup_job_server():
+    local('scp -i %s scrape-youtube.py %s@%s:.' %(env.key_filename, env.user, env.hosts[0]))
+    run('chmod +x ./scrape-youtube.py')
+
+@task
+@hosts('localhost')
+def deploy_video_clients():
+    for host in env.hosts:
+	local('scp -i %s video_client.py %s@%s:.' %(env.key_filename, env.user, host))
+
+@task
+def run_video_clients():
+    with settings(warn_only = True):
+	run('mkdir /tmp/downloads')
+    run('chmod +x ./video_client.py; ./video_client.py')
+
+@task
+def cleanup_video_client():
+    run('rm -rf /tmp/download/*')
+
+@task
+@hosts(env.hosts[0])
+def run_job_server():
+    run('./scrape-youtube.py')
+    
+    
+    
+
+
