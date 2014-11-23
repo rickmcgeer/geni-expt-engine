0be265289e03bf72dc91407e40c175dbeb0ed199
Rick McGeer <rick@mcgeer.com>
2014-11-22 18:32:42 -0800

    changed path to nodejs for modern environment

--- gee-master/run-gee-server.sh
+++ gee-master/run-gee-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export GEE_CONFIG_FILE='/home/service_instageni/test/config.py'
 while true; do
-  /usr/local/bin/node app.js >> gee_console.log 2>> gee_console_error.log
+  /usr/bin/nodejs app.js >> gee_console.log 2>> gee_console_error.log
   # When you get here the process has died.  start
   # the loop again and restart it
 done
