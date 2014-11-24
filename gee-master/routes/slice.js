db0f529265da6263b59b8dbde240f49dff844f6a
Rick McGeer <rick@mcgeer.com>
2014-11-24 14:30:39 -0800

    integrated with the create-slice.sh an ddelete-slice.sh scripts

--- gee-master/routes/slice.js
+++ gee-master/routes/slice.js
@@ -10,6 +10,51 @@ module.exports = function (app, utils, urls, url, Users, Slices, script_dir) {
         return expiration;
     }
     
+    // invoke a remote command.  If the command exits with 0, invoke callBack, which should
+    // be a function foo(req, res, callBackArgStruct, results), where results are the data
+    // that came back from command.  On error invoke errorCallBack, which takes (req, res, error)
+    
+    var invokeCommand = function(req, res, cmdName, argList, callBack, callBackArgStruct, errorCallback) {
+        var spawn = require('child_process').spawn;
+        var cmd = spawn(script_dir + '/' + cmdName, argList);
+        var error = "";
+        var result = "";
+        var data_received = false;
+        cmd.stdout.on('data', function (data) {
+            // console.log('stdout: ' + data);
+            result = JSON.parse(data);
+            data_received = true;
+        });
+        // when data is received on stderr, we have a problem and log it.  Should do something more
+        // intelligent on exit...
+        cmd.stderr.on('data', function (data) {
+            console.log('Error in ' + cmdName +': ' + data);
+            error = error + data;
+        });
+        // when the command finishes, either handle the error or do the callback
+        cmd.on('close', function (code) {
+            console.log('child process exited with code ' + code);
+            if(code > 0) {
+                errorCallback(req, res, "Error in subprocess " + cmdName + " " + argList + ": " + error)
+            } else {
+                callBack(req, res, callBackArgStruct, result)
+            }
+        });
+    }
+    
+    var deleteSliceFromDBOnError = function(req, res, errorMessage) {
+       Slices.remove({user:req.session.user}, function(err, result) {
+        if(err) {
+            utils.handleError(req, res, "Double Error, requires administrator attention: " + errorMessage + " : " + err)
+        } else {
+            utils.handleError(req, res, errorMessage)
+        }
+       })
+    }
+    var redirectToUser = function(req, res) {
+        res.redirect('/user')
+    }
+    
     // get the next slice number.  used only for debugging
     app.get('/slice/next', function (req, res) {
         res.end('Next slice number is ' + next_slice)
@@ -23,6 +68,7 @@ module.exports = function (app, utils, urls, url, Users, Slices, script_dir) {
                 utils.render_error_page(req, res,  message);
             } else {
                 var sliceName = utils.makeSliceName(nextSliceNum)
+                var tarFile = makeTarfile(sliceName)
                 Slices.create({user:req.session.user,
                               tarfile:makeTarfile(sliceName),
                               expires:makeExpiryDate()
@@ -33,8 +79,8 @@ module.exports = function (app, utils, urls, url, Users, Slices, script_dir) {
                                     console.log(message)
                                     utils.render_error_page(req, res, message)
                                 } else {
-                                    console.log("Slice " + sliceName + " created for user " + req.session.user)
-                                    res.redirect('/user')
+                                    console.log("Slice " + sliceName + " entered for user " + req.session.user)
+                                    invokeCommand(req, res, 'createSlice.sh', [sliceName, tarFile, 'foo'], redirectToUser, {}, deleteSliceFromDBOnError) // need to fix the imagename
                                 }
                               });
             }
@@ -44,16 +90,23 @@ module.exports = function (app, utils, urls, url, Users, Slices, script_dir) {
     // free a slicelet.  Just deletes the slicelet from the db
     app.get('/slice/free', function (req, res) {
         console.log(req.url);
-        Slices.remove({user:req.session.user}, function(err) {
+        Slices.find({user:req.session.user}, function(err, slices) {
             if(err) {
-                var message = "Error removing slice for user " + req.session.user + " from the database: " + err
-                console.log(message)
-                utils.render_error_page(req, res, message)
+                utils.handleError("Error in finding slice for user " + req.session.user + ": " + err)
+            } else if (slices.length == 0) {
+                utils.handleError("No slices found for user " + req.session.user)
             } else {
-                console.log("Slice   for user " + req.session.user + " freed ")
-                res.redirect('/user')
+                var sliceName = utils.makeSliceName(slices[0].sliceNum)
+                var tarfile = slices[0].tarfile;
+                Slices.remove({user:req.session.user}, function(err) {
+                    if(err) {
+                        utils.handleError("Error removing slice for user " + req.session.user + " from the database: " + err)
+                    } else {
+                        invokeCommand(req, res, 'deleteSlice.sh', [sliceName, tarFile], redirectToUser, {}, utils.handleError)
+                    }
+                })
             }
-        });
+        })
     });
     //
     // Renew a slicelet.  Just calls makeExpiryDate to set the session expiration two weeks into the
