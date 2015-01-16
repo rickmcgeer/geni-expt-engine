cf97e8b2a09a25e4a85f225054c9fb4e4c19414d
Andy Bavier <acb@cs.princeton.edu>
2015-01-16 18:36:01 +0000

    Changes for production GEE

--- slice-scripts/delete-slice.sh
+++ slice-scripts/delete-slice.sh
@@ -6,4 +6,4 @@ ANSIBLEDIR=$DIR/../ansible
 
 [ $# -eq 0 ] && { echo "Usage: $0 slice_name"; exit 1; }
 
-ansible-playbook -i $ANSIBLEDIR/hosts $ANSIBLEDIR/delete-slice.yml --extra-vars "slice=$SLICE"
+ansible-playbook -f 20 -i $ANSIBLEDIR/hosts $ANSIBLEDIR/delete-slice.yml --extra-vars "slice=$SLICE"
