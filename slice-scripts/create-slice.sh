d2f7107b0ecee00084be109c62c25a795f3da6c0
Andy Bavier <acb@cs.princeton.edu>
2015-02-04 15:17:30 -0500

    Log Ansible output to /var/log/gee

--- slice-scripts/create-slice.sh
+++ slice-scripts/create-slice.sh
@@ -4,10 +4,14 @@ SLICE=$1
 TARBALL=$2
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ANSIBLEDIR=$DIR/../ansible
+LOGDIR=/var/log/gee
+LOGFILE=$LOGDIR/$SLICE-create.log
 
 [ $# -eq 0 ] && { echo "Usage: $0 slice_name"; exit 1; }
 
-ansible-playbook -f 20 -i $ANSIBLEDIR/hosts $ANSIBLEDIR/create-slice.yml --extra-vars "slice=$SLICE tarball=$TARBALL"
+mkdir -p $LOGDIR
+echo Run: `date` >> $LOGFILE
+ansible-playbook -f 20 -i $ANSIBLEDIR/hosts $ANSIBLEDIR/create-slice.yml --extra-vars "slice=$SLICE tarball=$TARBALL" >> $LOGFILE
 
 # Tolerate dark hosts (exit code 3 from Ansible)
 RESULT=$?
--- slice-scripts/delete-slice.sh
+++ slice-scripts/delete-slice.sh
@@ -3,10 +3,14 @@
 SLICE=$1
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ANSIBLEDIR=$DIR/../ansible
+LOGDIR=/var/log/gee
+LOGFILE=$LOGDIR/$SLICE-delete.log
 
 [ $# -eq 0 ] && { echo "Usage: $0 slice_name"; exit 1; }
 
-ansible-playbook -f 20 -i $ANSIBLEDIR/hosts $ANSIBLEDIR/delete-slice.yml --extra-vars "slice=$SLICE"
+mkdir -p $LOGDIR
+echo Run: `date` >> $LOGFILE
+ansible-playbook -f 20 -i $ANSIBLEDIR/hosts $ANSIBLEDIR/delete-slice.yml --extra-vars "slice=$SLICE" >> $LOGFILE
 
 # Tolerate dark hosts (exit code 3 from Ansible)
 RESULT=$?
