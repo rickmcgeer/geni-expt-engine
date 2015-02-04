b637c85ecf20524af4bbf3cb8efe9b91c9a5eca1
Andy Bavier <acb@cs.princeton.edu>
2015-02-04 14:29:01 -0500

    Tolerate dark hosts

--- slice-scripts/create-slice.sh
+++ slice-scripts/create-slice.sh
@@ -8,3 +8,8 @@ ANSIBLEDIR=$DIR/../ansible
 [ $# -eq 0 ] && { echo "Usage: $0 slice_name"; exit 1; }
 
 ansible-playbook -f 20 -i $ANSIBLEDIR/hosts $ANSIBLEDIR/create-slice.yml --extra-vars "slice=$SLICE tarball=$TARBALL"
+
+# Tolerate dark hosts (exit code 3 from Ansible)
+RESULT=$?
+[ $RESULT -eq 3 ] && { exit 0; }
+exit $RESULT
--- slice-scripts/delete-slice.sh
+++ slice-scripts/delete-slice.sh
@@ -7,3 +7,8 @@ ANSIBLEDIR=$DIR/../ansible
 [ $# -eq 0 ] && { echo "Usage: $0 slice_name"; exit 1; }
 
 ansible-playbook -f 20 -i $ANSIBLEDIR/hosts $ANSIBLEDIR/delete-slice.yml --extra-vars "slice=$SLICE"
+
+# Tolerate dark hosts (exit code 3 from Ansible)
+RESULT=$?
+[ $RESULT -eq 3 ] && { exit 0; }
+exit $RESULT
