cbf4e8e108de03fce14fce74651ba2a2731a907b
Rick McGeer <rick@mcgeer.com>
2016-08-14 09:35:40 -0700

    modified to produce more detailed information under the --host or --hostAll options, and made --generateInventory a mutually exclusive option

--- ansible/dynInventory.py
+++ ansible/dynInventory.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 #
 # Script to get the host inventory for the GEE dynamically from the node collection in the database.  Designed to be called by
-# ansible, so the usage is dictacted by Ansible.  See: http://docs.ansible.com/ansible/intro_dynamic_inventory.html and
+# ansible, so the usage is dictated by Ansible.  See: http://docs.ansible.com/ansible/intro_dynamic_inventory.html and
 # http://docs.ansible.com/ansible/developing_inventory.html
 # To use with ansible, use in place of a static file, e.g.
 # ansible -i dynInventory.py nodes -m ping
@@ -10,21 +10,24 @@
 #     returns a JSON dictionary of lists of hosts
 # dynInventory --host hostname
 #     returns information about host hostname as a dictionary
-# Currently, the --list form only returns one entry, 'nodes', and --host hostname always returns an empty dictionary
-# To these options, added the option --generateInventory.  If called with this and the --list flag, will generate 
-# and print an Ansible inventory file.  This  is to generate the Ansible files to be bundled with the slicelet tarball.
+# dynInventory --hostAll
+#     returns detailed information about all host
+# dynInventor --generateInventory
+#     prints an Ansible inventory file. This  is to generate the Ansible files to be bundled with the slicelet tarball.
+# Currently, the --list form only returns one entry, 'nodes'. 
 #
 import argparse
 import json
 from pymongo import MongoClient, ReturnDocument
 def parse_args():
     parser = argparse.ArgumentParser(description='Dynamic Inventory From GEE Database')
-    parser.add_argument('--generateInventory', action='store_true', default=False,
-                        help='Generate a static inventory file')
     group = parser.add_mutually_exclusive_group(required=True)
+    parser.add_argument('--generateInventory', action='store_true',
+                        help='Generate a static inventory file')
     group.add_argument('--list', action='store_true',
                        help='List active servers')
-    group.add_argument('--host', help='List details about the specific host')
+    group.add_argument('--host', nargs='+', help='List details about the specific host')
+    group.add_argument('--hostAll', action = 'store_true', help='List details about all hosts')
     return parser.parse_args()
 
 client = MongoClient('mongodb://mongodb:27017/')
@@ -36,25 +39,27 @@ node_collection = db.nodes
 def getAllNodes():
     nodes = node_collection.find({})
     return [node['dnsName'] for node in nodes]
+
+
 #
 # get the result dictionary
 #
 def getDictionary(args):
-    if args.list:
-        return {'nodes': getAllNodes()}
-    elif args.host:
-        return {}
+    if (args.host):
+        specifier = {'$in': args.host}
+    else:
+        specifier = {}
+    return node_collection.find({})
 
 if __name__ == '__main__':
     args = parse_args()
-    if args.list:
-        result = {'nodes': getAllNodes()}
+    nodes  = getDictionary(args)
+    if (args.host or args.hostAll):
+        print json.dumps(nodes)
+    elif args.generateInventory:
+        print '[nodes]'
+        for node in nodes: print node['dnsName']
     else:
-        result = {}
-    if args.generateInventory:
-        if 'nodes' in result:
-            print '[nodes]'
-            for node in result['nodes']: print node
-    else:
+        result = {'nodes': [node['dnsName'] for node in nodes]}
         print json.dumps(result)
 
