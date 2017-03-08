ee237f70cc672ea2fb653123c997bf1fe9334bce
Rick McGeer <rick@mcgeer.com>
2017-03-07 19:10:16 -0800

    updated DNS server to ground truth every 10 minutes

--- etc/updateDNSDaemon.py
+++ etc/updateDNSDaemon.py
@@ -4,11 +4,18 @@ execfile('namecheap-lib.py')
 
 if __name__ == '__main__':
     lastHosts = []
+    count = 0
     while True:
         newHosts = hostsFromDB()
         if (len(newHosts) != len(lastHosts)):
             doUpdate(newHosts)
+            count = 0
         elif (listsChanged(newHosts, lastHosts)):
-                doUpdate(newHosts)
+            doUpdate(newHosts)
+            count = 0
+        elif (count == 10):
+            doUpdate(newHosts)
+            count = 0
+        else: count += 1
         lastHosts = newHosts
         time.sleep(60)
