af7de0d0677cad16817bf640eaa2f5ef0c20f256
Matthew Hemmings <discount.yoyos@gmail.com>
2017-07-19 10:52:10 -0700

    Enabled commented out lines to start mongodb and sleep statement

--- create-production-portal.sh
+++ create-production-portal.sh
@@ -12,8 +12,8 @@ usage(){
 TAG=$1
 PORT=$2
 
-# docker run -d --name $TAG-mongodb geeproject/gee_db_server
+docker run -d --name $TAG-mongodb geeproject/gee_db_server
 
-# sleep 10
+sleep 10
 
 docker run -d --name $TAG-portal -p $PORT:$PORT --link $TAG-mongodb:mongodb -v /root/ssh:/root/.ssh:ro -v /root/slice_files:/root/slice_files portal
